4c1295b9ffdba88a318a9b4f0c2a1e50af7d731b
Robin Garcia <robin.garcia-victoria@ist.uni-stuttgart.de>
2016-12-13 11:03:35 +0100

    Example added

---
+++ mass_spring_system.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 13 10:44:02 2016
+
+@author: robin
+"""
+
+from scipy.integrate import odeint
+
+def MassSpring(state, t):
+  # unpack the state vector
+  x = state[0]
+  xd = state[1]
+
+  # these are our constants
+  k = -2.5 # Newtons per metre
+  m = 1.5 # Kilograms
+  g = 9.8 # metres per second
+
+  # compute acceleration xdd
+  xdd = ((k*x)/m) + g
+
+  # return the two state derivatives
+  return [xd, xdd]
+
+state0 = [0.0, 0.0]
+t = arange(0.0, 10.0, 0.1)
+
+state = odeint(MassSpring, state0, t)
+
+plot(t, state)
+xlabel('TIME (sec)')
+ylabel('STATES')
+title('Mass-Spring System')
+legend(('$x$ (m)', '$\dot{x}$ (m/sec)'))
--- neg_autoregulation.py
+++ neg_autoregulation.py
@@ -6,5 +6,8 @@ Created on Tue Dec 13 10:09:54 2016
 @author: robin
 """
 
-## Implementation of negative autoregulation
+# Implementation of negative autoregulation
 
+A = 1
+A_e = 1
+z = A-A_e

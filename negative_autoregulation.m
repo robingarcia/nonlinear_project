eb7ee54da007e0149e63ff0112f16805ba62735b
Robin Garcia <robin.garcia-victoria@ist.uni-stuttgart.de>
2017-01-13 14:34:02 +0100

    implement ode

--- negative_autoregulation.m
+++ negative_autoregulation.m
@@ -6,13 +6,20 @@
 % Model class for NAR
 
 
-% Parameters with IC
+% Definition of Parameters
 n = 1;
 K = 0.1;
 a = 1;
 b = 1;
+tspan = [0 3];
+X0=0;
+
 
 % ODE NAR
-X = sym('X');
-t = sym('t');
-dX/dt = b*(1/(1+(X/K)^n))-a*X;
+%dX/dt = b*(1/(1+(X/K)^n))-a*X;
+%y = dsolve((diff(X)==b*(1/(1+(X/K)^n))-a*X));
+[t,X] = ode45(@(t,X) b*(1/(1+(X/K)^n))-a*X, tspan, X0);
+plot(t,X,'-o')
+title('Solution of negative autoregulation with ODE45');
+xlabel('Time t');
+ylabel('Solution X');
